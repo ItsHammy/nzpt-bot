5e9f5ee93125bf1181fdfb61b7df9ef13bdbe825
CJ Sandall <cj@cjs.nz>
2026-05-06 11:23:30 +1200

    Basic Bot

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 import os
 
-from atproto import Client
+from atproto import Client, models
 from dotenv import load_dotenv
 import time
 
@@ -11,6 +11,9 @@ load_dotenv()
 BLUESKY_USERNAME = os.getenv("BLUESKY_USERNAME")
 BLUESKY_PASSWORD = os.getenv("BLUESKY_PASSWORD")
 
+# CONSTANTS
+REPLY_DISCLAIMER = "This post is automatically posted by NZPT. See nzpt.cjs.nz for the stats, or contact the account in bio to report any issues."
+
 # Create a Bluesky client
 client = Client("https://bsky.social")
 
@@ -19,16 +22,21 @@ def update(type, text):
     client.login(BLUESKY_USERNAME, BLUESKY_PASSWORD)
     print(f"Logged in as {BLUESKY_USERNAME} at {time.ctime()}")
     if type == "auto":
-        if len(text) > 280:
-            print("Error: Text exceeds 280 characters for auto post.")
+        if len(text) > 286:  # 280 characters + 6 for "[AUTO] " and " #NZPol"
+            print("Error: Text exceeds 286 characters for auto post.")
             return
         else:
-            client.post("[AUTO] " + text)
+            post_response = client.send_post("[AUTO] " + text + " #NZPol")
+
+            parent_ref = models.create_strong_ref(post_response)
+            reply_ref = models.AppBskyFeedPost.ReplyRef(parent=parent_ref, root=parent_ref)
+            client.send_post(REPLY_DISCLAIMER, reply_to=reply_ref)
+            print("Disclaimer reply posted.")
     elif type == "manual":
-        if len(text) > 280:
-            print("Error: Text exceeds 280 characters for manual post.")
+        if len(text) > 300:
+            print("Error: Text exceeds 300 characters for manual post.")
             return
-        client.post(text)
+        client.send_post(text)
     else:
         print("Error: Invalid post type. Use 'auto' or 'manual'.")
 
